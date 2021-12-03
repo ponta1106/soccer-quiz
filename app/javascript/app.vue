aa3cdf52377ab0ae5749f797ea7dd99e829f6ed7
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2021-12-03 19:04:33 +0900

    Add: Vue.jsを導入して、トップページとクイズ一覧ページの雛形を作成しました。

---
+++ app/javascript/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+#app {
+  color: #555;
+  min-height: 100vh;
+}
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
