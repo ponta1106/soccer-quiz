c6d9e7a33695a110910e299b57a35002307d1e8a
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2021-12-10 13:13:41 +0900

    Revert "Improve: scssの設定を一部変更"
    This reverts commit 57b7ae6919e2bdc24253d5ecbdf35971560801a3.

--- app/javascript/pages/question/index.vue
+++ app/javascript/pages/question/index.vue
@@ -86,7 +86,7 @@ export default {
       isVisibleChampionsLeagueQuestionModal: false,
       isVisibleSerieAQuestionModal: false,
       isVisiblePremierLeagueQuestionModal: false,
-      isVisibleQuestionCreateModal: true,
+      isVisibleQuestionCreateModal: false,
     }
   },
   computed: {
