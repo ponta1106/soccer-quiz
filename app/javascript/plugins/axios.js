3878f3f20414b68d43ff95e061f6bd01177b32ca
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2021-12-20 15:19:50 +0900

    Add: baseURLにスラッシュを追加

--- app/javascript/plugins/axios.js
+++ app/javascript/plugins/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: 'api'
+  baseURL: '/api'
 })
 
-// if (localStorage.auth_token) {
-//   axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.auth_token}`
-// }
+if (localStorage.auth_token) {
+  axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.auth_token}`
+}
 
 export default axiosInstance
