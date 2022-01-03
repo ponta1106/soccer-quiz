6a0b135b3c3b57bd3e435b443a53745486c1555d
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2022-01-04 00:49:54 +0900

    Add: クイズのカテゴリーを追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :played_questions, dependent: :destroy
   has_many :correct_answers, dependent: :destroy
-  enum category: { champions_league: 0, serieA: 1, premier_league: 2, la_liga: 3, others: 4 }
+  enum category: { champions_league: 0, serieA: 1, premier_league: 2, la_liga: 3, bundes_liga: 4, league_1: 5, others: 6 }
 
   with_options presence: true do
     validates :title
